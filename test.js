5d3e76b5ed9af34f8542dc138653021b8e0ae156
Mathieu Lecarme <mathieu@garambrogne.net>
2011-02-16 21:50:52 +0100

    meeting

--- test.js
+++ test.js
@@ -4,5 +4,13 @@ var conf = require('./conf').Conf,
 var uc = new Ucengine(conf);
 
 uc.time(function(resp) {
-	console.log(resp);
+	console.log('time: ', resp);
+});
+
+uc.meeting(function(resp) {
+	console.log('meeting: ', resp);
+});
+
+uc.presence(conf.uid, conf.credential, function(resp) {
+	console.log('presence :', resp);
 });
--- ucengine.js
+++ ucengine.js
@@ -1,4 +1,5 @@
-var http = require('http');
+var http = require('http'),
+	querystring = require('querystring');
 
 var Ucengine = function(conf) {
 	this.host = conf.host;
@@ -7,14 +8,19 @@ var Ucengine = function(conf) {
 
 exports.Ucengine = Ucengine;
 
-Ucengine.prototype.auth = function(login, password) {
-	
+Ucengine.prototype._request_opts = function(method, path) {
+	//console.log('path: ', '/api/0.3' + path);
+	return {
+		host: this.host,
+		port: this.port,
+		path: '/api/0.3' + path,
+		method: method};
 };
 
-Ucengine.prototype.time = function(cb) {
-	var req = http.request({host: this.host, port: this.port, path: '/api/0.3/time', method: 'GET'}, function(res) {
-		//console.log('STATUS: ' + res.statusCode);
-		//console.log('HEADERS: ' + JSON.stringify(res.headers));
+Ucengine.prototype._request = function(method, path, body, cb) {
+	var req = http.request(this._request_opts(method, path), function(res) {
+		console.log('STATUS: ' + res.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(res.headers));
 		res.setEncoding('utf8');
 		var response = "";
 		res.on('data', function (chunk) {
@@ -23,8 +29,26 @@ Ucengine.prototype.time = function(cb) {
 		res.on('end', function() {
 			cb(JSON.parse(response));
 		});
-	}).end();
+	});
+	if(body != null) {
+		//[FIXME] if object => querystring.stringify, String, Buffer => raw
+		req.write(querystring.stringify(body));
+	}
+	req.end();
+	
 };
+
+Ucengine.prototype.presence = function(uid, credential, cb) {
+	this._request('POST', '/presence/', {uid: uid, credential:credential, "metadata[nickname]": uid }, cb);
+};
+
+Ucengine.prototype.time = function(cb) {
+	this._request('GET', '/time', null, cb);
+};
+
+Ucengine.prototype.meeting = function(cb) {
+	this._request('GET', '/meeting/opened', null, cb);
+}
 
 Ucengine.prototype.user = function() {
 	var req = http.request({host: this.host, port: this.port, path: '/user/', method: 'GET'}, function(res) {
