7950a2084e53ff4ffa42d01bf8e47dd9f881ec2a
Mathieu Lecarme <mathieu@garambrogne.net>
2011-02-24 21:38:18 +0100

    batch demo

--- test/demo.js
+++ test/demo.js
@@ -1,34 +1,17 @@
 var uce = require('../lib/ucengine'),
 	Ucengine = uce.Ucengine,
-	User = uce.User;
+	User = uce.User,
+	Meeting = uce.Meeting;
 
 var uc = new Ucengine({host: 'localhost', port:5280});
 
-var logins = [
-	"thierry.bomandouki@af83.com",
-	"victor.goya@af83.com",
-	"louis.ameline@af83.com",
-	"alexandre.eisenchteter@af83.com",
-	"romain.gauthier@af83.com",
-	"participant"
-];
-
-var users = {};
-
-
-function moreUser(i, cb) {
-	uc.create(new User(logins[i], 'pwd'), function(resp, u) {
-		u.presence(function(resp) {
-			users[u.uid] = u;
-			if(i <= logins.length - 2) {
-				moreUser(i+1, cb);
-			} else {
-				cb.call();
-			}
-		});
-	});
-};
-
-moreUser(0, function() {
+uc.batchUsers([
+	["thierry.bomandouki@af83.com", 'pwd'],
+	["victor.goya@af83.com", "pwd"],
+	["louis.ameline@af83.com", "pwd"],
+	["alexandre.eisenchteter@af83.com", "pwd"],
+	["romain.gauthier@af83.com", "pwd"],
+	["participant", "pwd"]
+], function(users) {
 	console.log(users);
 });
