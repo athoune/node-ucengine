3c5b7bdddcd238fabbe20c53114ba678703dbd0b
Mathieu Lecarme <mathieu@garambrogne.net>
2011-02-24 23:40:53 +0100

    async batch user add

--- test/demo.js
+++ test/demo.js
@@ -5,13 +5,29 @@ var uce = require('../lib/ucengine'),
 
 var uc = new Ucengine({host: 'localhost', port:5280});
 
-uc.batchUsers([
+var agoroom = new Meeting("agoroom");
+
+var users = {};
+uc.asyncUsers([
 	["thierry.bomandouki@af83.com", 'pwd'],
 	["victor.goya@af83.com", "pwd"],
 	["louis.ameline@af83.com", "pwd"],
 	["alexandre.eisenchteter@af83.com", "pwd"],
 	["romain.gauthier@af83.com", "pwd"],
 	["participant", "pwd"]
-], function(users) {
-	console.log(users);
+], function(user) {
+	users[user.uid] = user;
+}, function() {
+	var oneMoreTime = function(ls) {
+		var lp = ls.pop();
+		lp.join(agoroom, function() {
+			console.log("meeting for " + lp.uid);
+			if(ls.length) {
+				oneMoreTime(ls);
+			} else {
+				console.log("everybody are in the meeting");
+			}
+		});
+	};
+	oneMoreTime(Object.keys(users).map(function(e) { return users[e];}));
 });
