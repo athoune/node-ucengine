3055d7f5cdd2b6cd5140d286659c6753494200d8
Mathieu Lecarme <mathieu@garambrogne.net>
2011-03-17 21:06:03 +0100

    nodeunit + infos, time as 0.4

--- test/test.js
+++ test/test.js
@@ -3,12 +3,38 @@ var conf = require('./conf').Conf,
 	Ucengine = uce.Ucengine,
 	User = uce.User;
 
-var uc = new Ucengine(conf);
+function UCcontext(cb) {
+	var uc = new Ucengine(conf);
+	var u = new User("participant", "pwd");
+	uc.attach(u, cb);
+}
 
-uc.time(function(resp) {
-	console.log('time: ', resp);
-});
+// UCcontext(, function() {
+// 	var uc = this.ucengine;
+// 	this.time(function(resp) {
+// 		uc.logger.log('time: ', resp);
+// 	});
+// 	this.infos(function(resp) {
+// 		uc.logger.log('infos: ', resp);
+// 	});
+// });
 
-uc.infos(function(resp) {
-	console.log('infos: ', resp);
-});
+exports.testTime = function(test) {
+	test.expect(1);
+	UCcontext(function() {
+		this.time(function(resp) {
+			test.ok(resp.result != null);
+			test.done();
+		});
+	});
+};
+
+exports.testInfos = function(test) {
+	test.expect(1);
+	UCcontext(function() {
+		this.infos(function(resp) {
+			test.equal("localhost", resp.result.domain);
+			test.done();
+		});
+	});
+};
